3524a644f3424ef33a4214a2f7b3aed8b239f5fc
HeeHeeg <qmffndoden@naver.com>
2023-08-23 10:20:32 +0900

    montor 페이지, 세부 페이지

--- src/routes/MyRouter.jsx
+++ src/routes/MyRouter.jsx
@@ -4,6 +4,10 @@ import Main from '../components/Main';
 import Template from '../template/Template';
 import KakaoCallBack from '../components/KakaoCallBack';
 import LoginInfo from '../components/LoginInfo';
+import Mentors from "../components/Mentors";
+import MentorRequests from "../components/MentorRequests";
+import MentorDetail from "../components/MentorDetail";
+
 
 const MyRouter = () => {
 
@@ -12,6 +16,9 @@ const MyRouter = () => {
             <Route path="/" element={<Main />} />
             <Route path="logininfo" element={<LoginInfo />} />
             <Route path="/login/kakao/callback" element={<KakaoCallBack />}></Route>
+            <Route path='/mentors' element={<Mentors/>}/>
+            <Route path='/mentorRequests' element={<MentorRequests/>}/> {/*  멘토 지원 페이지 */}
+            <Route path="/mentorDetail" element={<MentorDetail/>}></Route>
         </Route>
     </Routes>
 
