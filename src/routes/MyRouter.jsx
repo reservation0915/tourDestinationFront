77a827a6bbe6695086a38f50bf1232f627f4434b
xodndlee <dnd5436@gmail.com>
2023-08-23 10:44:26 +0900

    111

--- src/routes/MyRouter.jsx
+++ src/routes/MyRouter.jsx
@@ -4,12 +4,14 @@ import Main from '../components/Main';
 import Template from '../template/Template';
 import KakaoCallBack from '../components/KakaoCallBack';
 import LoginInfo from '../components/LoginInfo';
+import ReactCarousel from "../bootstrap/ReactCrousel";
+import Home from "../components/main/Home";
 
 const MyRouter = () => {
 
     return <Routes>
         <Route element={<Template />}>
-            <Route path="/" element={<Main />} />
+            <Route path='/' element={<Home />}></Route>
             <Route path="logininfo" element={<LoginInfo />} />
             <Route path="/login/kakao/callback" element={<KakaoCallBack />}></Route>
         </Route>
